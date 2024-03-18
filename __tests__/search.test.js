215a95cad799c6bf4430e51c37361867824b06c0
ShcherbinaDmitry <shcherbinadmitry@gmail.com>
2024-03-18 09:59:26 +0300

    feat: sort results by relevance

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -9,6 +9,18 @@ test('search', () => {
   expect(search([{ id: 1, text: 'a' }, { id: 2, text: 'b' }], 'a')).toEqual([1]);
   expect(search([{ id: 1, text: 'a' }, { id: 2, text: 'b' }], 'b')).toEqual([2]);
   expect(search([{ id: 1, text: 'a' }, { id: 2, text: 'b' }], 'c')).toEqual([]);
-  expect(search([{ id: 1, text: 'a' }, { id: 2, text: 'ab' }], 'a')).toEqual([1, 2]);
+  expect(search([{ id: 1, text: 'a' }, { id: 2, text: 'ab' }], 'a')).toEqual([1]);
+});
+
+test('search with regex', () => {
   expect(search([{ id: 1, text: 'test' }, { id: 2, text: 'foo' }], 'test!')).toEqual([1]);
+});
+
+test('search with relevance', () => {
+  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+  const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+  const doc3 = { id: 'doc3', text: "I'm your shooter." };
+  const doc4 = { id: 'doc4', text: "Please, don't shoot or I will shoot you!" };
+
+  expect(search([doc1, doc2, doc3, doc4], 'shoot')).toEqual(['doc2', 'doc4', 'doc1']);
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const search = (items, token) => {
   const term = token.match(/\w+/g)[0];
+  const regex = new RegExp(`\\b${term}\\b`, 'gmi');
 
   return items
-    .filter(({ text }) => text.toLowerCase().includes(term))
+    .map((item) => {
+      const { id, text } = item;
+      const relevance = (text.match(regex) || []).length;
+      return { id, text, relevance };
+    })
+    .filter(({ relevance }) => relevance)
+    .sort((a, b) => b.relevance - a.relevance)
     .map(({ id }) => id);
 };
 
